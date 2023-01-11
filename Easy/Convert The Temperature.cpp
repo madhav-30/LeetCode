4d0b5eb9d6ad8126ebb0acccc541553b0c6238c0
Madhav Bhardwaj <114002111+madhav-30@users.noreply.github.com>
2023-01-11 12:57:15 +0530

    Create Convert The Temperature.cpp

---
+++ Easy/Convert The Temperature.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double Kelvin = celsius + 273.15;
+        double Fahrenheit = celsius *1.80 + 32.00;
+        vector <double>Total;
+        Total.push_back(Kelvin);
+        Total.push_back(Fahrenheit);
+        return Total;
+    }
+};
